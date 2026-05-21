9734519ac0bc64af5f9a62fc3d13690a11db1ea1
ho8566z <ho8566z@gmail.com>
2026-05-21 17:06:36 +0900

    20260521 at_class: finished, commit-005_2

---
+++ 20260521ex/oneDiary/member/member_dumy.py
@@ -0,0 +1,16 @@
+from db import member_db
+
+ids = ['gildong', 'chanho']
+pws = ['1234', '5678']
+mails = ['gildong@gmail.com', 'chanho@naver.com']
+phones = ['010-0001-0002', '010-0011-0022']
+
+
+def memberDumtInit():
+    for n in range(len(ids)):
+        member_db.memberDB[ids[n]] = {
+            'uId': ids[n],
+            'uPw': pws[n],
+            'uMail': mails[n],
+            'uPhone':phones[n]
+        }
